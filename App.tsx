33bfae78bcc1bfa731fac7ceb27692fb353f5753
tomasmach <tomades1@gmail.com>
2026-04-20 11:27:37 +0200

    feat: wrap app in SafeAreaProvider

--- App.tsx
+++ App.tsx
@@ -1,13 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import { TimerScreen } from './screens/TimerScreen';
 import { colors } from './constants/colors';
 
 export default function App() {
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: colors.background }}>
-      <StatusBar style="light" />
-      <TimerScreen />
-    </SafeAreaView>
+    <SafeAreaProvider>
+      <SafeAreaView style={{ flex: 1, backgroundColor: colors.background }}>
+        <StatusBar style="light" />
+        <TimerScreen />
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
